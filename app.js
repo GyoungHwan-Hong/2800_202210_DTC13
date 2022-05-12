8610aebe980a8bbd2200774bd1bf5343cdd24e3f
GyoungHwan-Hong <hongkh5218@gmail.com>
2022-05-12 14:44:13 -0700

    Adds recipe.html

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const button=()=> {
-    const burger = document.querySelector('.hamburger_menu');
+    const burger = document.querySelector('.burger');
     burger.addEventListener('click',()=>{
         burger.classList.toggle('toggle');
     });
 }
 
 button();
+
+
+
+function Trendy_contents_change_Today() {
+    document.getElementById("Trendy_Food_List").innerHTML = "<li>Kimchi</li><li>Curry rice</li><li>Pasta</li><li>Pizza</li><li>Coffee with Donut</li>";
+}
+
+function Trendy_contents_change_Weekly() {
+    document.getElementById("Trendy_Food_List").innerHTML = "<li>Shushi</li><li>Curry rice</li><li>Pasta</li><li>Pizza</li><li>Coffee with Donut</li>";
+}
+
+function Trendy_contents_change_Monthly() {
+    document.getElementById("Trendy_Food_List").innerHTML = "<li>Pho</li><li>Curry rice</li><li>Pasta</li><li>Pizza</li><li>Coffee with Donut</li>";
+}
