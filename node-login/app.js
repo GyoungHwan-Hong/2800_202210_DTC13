7d9528cdafbd87e06d02eaaea94b91017c46e2b3
Bryan Tran <bryant.business@hotmail.com>
2022-05-18 09:27:35 -0700

    Inital Commit

---
+++ node-login/app.js
@@ -0,0 +1,21 @@
+
+/**
+	* Node.js Login Boilerplate
+	* https://node-login.braitsch.io
+	* (c) 2013-2021 Stephen Braitsch
+**/
+
+//	https://www.npmjs.com/package/@braitsch/express
+
+
+const express = require('@braitsch/express');
+
+const app = express();
+
+express.log('./logs');
+
+express.http(app);
+
+express.init(__dirname, app, 'test', true);
+
+express.start(app);
