a0d3e107ba1c21c9713f37a6a8310a7275c15330
GyoungHwan-Hong <hongkh5218@gmail.com>
2022-05-18 09:49:29 -0700

    Add some ejs stuff to implement dynamic response.

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-const express = require('express')
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const app = express()
 const https = require('https')
+const bodyparser = require("body-parser");
+
+app.use(bodyparser.urlencoded({
+    parameterLimit: 100000,
+    limit: '50mb',
+    extended: true
+}))
 
 
 app.set('view engine', 'ejs');
@@ -9,3 +18,27 @@ app.listen(process.env.PORT || 5000, function (err) {
 })
 
 app.use(express.static('./public'));
+
+
+app.use('/recipe/:id', function (req, res) {
+
+    const url = `https://api.spoonacular.com/recipes/${req.params.id}/information?apiKey=598dbdb711b34618b52ffcd93f1e1104&includeNutrition=false`
+    data = ""
+
+    https.get(url, function (https_res) {
+        https_res.on("data", function (chunk) {
+            data += chunk
+        })
+
+        https_res.on("end", function () {
+            data = JSON.parse(data)
+            //console.log("Name: ", data.name)
+
+            res.render("recipe.ejs", {
+                "summary": data.summary,
+                "title": data.title,
+                "food_image": data.image
+            });
+        })
+    })
+})
