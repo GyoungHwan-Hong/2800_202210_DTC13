0f8601a41da4365a57276a2980e8c4223d8d060a
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-18 16:20:11 -0700

    change directory and links to css files for ejs pages in the project

--- server.js
+++ server.js
@@ -12,31 +12,28 @@ app.use(bodyparser.urlencoded({
 }))
 
 
-app.set('view engine', 'ejs');
 app.listen(process.env.PORT || 5000, function (err) {
     if (err) console.log(err);
 })
 
+app.set('view engine', 'ejs');
+
 app.use(express.static('./public'));
 
 
-app.use('/css', express.static(__dirname + 'public/css'))
-
-
 app.use('/recipe/:id', function (req, res) {
-
     const url = `https://api.spoonacular.com/recipes/${req.params.id}/information?apiKey=44bee3db3d864814b2a115572ee2f5f4&includeNutrition=false`
     data = ""
-
+    
     https.get(url, function (https_res) {
         https_res.on("data", function (chunk) {
             data += chunk
         })
-
+    
         https_res.on("end", function () {
             data = JSON.parse(data)
             //console.log("Name: ", data.name)
-
+    
             res.render("recipe.ejs", {
                 "summary": data.summary,
                 "title": data.title,
