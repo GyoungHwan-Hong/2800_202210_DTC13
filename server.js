62942b96aaa21e7e812c84b3c349309381bbb864
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-18 17:56:54 -0700

    resolve conflicts from dev

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.listen(process.env.PORT || 5000, function (err) {
     if (err) console.log(err);
 })
 
-<<<<<<< HEAD
 app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
@@ -31,17 +30,6 @@ app.use('/recipe/:id', function (req, res) {
     const url = `https://api.spoonacular.com/recipes/${req.params.id}/information?apiKey=44bee3db3d864814b2a115572ee2f5f4&includeNutrition=false`
     data = ""
     
-=======
-app.use(express.static(__dirname + '/public'));
-
-
-app.use('/recipe/:id', function (req, res) {
-
-    const url = `https://api.spoonacular.com/recipes/${req.params.id}/information?apiKey=69f6b2d77b0e498f9c58c444875354ab&includeNutrition=false`
-    data = ""
-    end =""
-
->>>>>>> f2d44159c7fce98c7b4f4982ad8b4a684574a8bd
     https.get(url, function (https_res) {
         https_res.on("data", function (chunk) {
             data += chunk
@@ -49,12 +37,8 @@ app.use('/recipe/:id', function (req, res) {
     
         https_res.on("end", function () {
             data = JSON.parse(data)
-<<<<<<< HEAD
             //console.log("Name: ", data.name)
     
-=======
-
->>>>>>> f2d44159c7fce98c7b4f4982ad8b4a684574a8bd
             res.render("recipe.ejs", {
                 "summary": data.summary,
                 "title": data.title,
