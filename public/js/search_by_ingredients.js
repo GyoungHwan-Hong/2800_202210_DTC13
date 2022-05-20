0fb54043c1eb7addace881d70e04a0bb597df54e
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-19 23:06:15 -0700

    modify Easter egg message and prepare to merge to dev

--- public/js/search_by_ingredients.js
+++ public/js/search_by_ingredients.js
@@ -24,24 +24,24 @@ async function search_by_ingredients() {
     // console.log(thirdItem);
 
     if (firstItem = secondItem = thirdItem){
-        alert("What a troll");
+        alert("Three of a kind! You've found an Easter Egg!");
     }
 
     if (cuisineItem = "Alien"){
         alert("There's no Alient type cuisine :p");
     }
 
-    // await $.ajax({
-    //     type: "get",
-    //     url: `https://api.spoonacular.com/recipes/findByIngredients?apiKey=69f6b2d77b0e498f9c58c444875354ab&ingredients=${firstItem},+${secondItem},+${thirdItem}&number=2`,
-    //     success: async function (data) {
-    //         console.log("GET request to Spoonacular API made");
-    //         for (i = 0; i < data.length; i++) {
-    //             console.log(data[i]["title"]);
-    //             $("results").append("<div>" + `${data[i]["title"]}` + "</div>");
-    //         }
-    //     }
-    // })
+    await $.ajax({
+        type: "get",
+        url: `https://api.spoonacular.com/recipes/findByIngredients?apiKey=69f6b2d77b0e498f9c58c444875354ab&ingredients=${firstItem},+${secondItem},+${thirdItem}&number=10`,
+        success: async function (data) {
+            console.log("GET request to Spoonacular API made");
+            for (i = 0; i < data.length; i++) {
+                console.log(data[i]["title"]);
+                $("results").append("<div>" + `${data[i]["title"]}` + "</div>");
+            }
+        }
+    })
 }
 
 
