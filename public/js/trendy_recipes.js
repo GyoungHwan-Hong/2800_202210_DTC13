14047e2ff7c4d2803c2a5c891741a25d10c7ad54
GyoungHwan Hong <hongkh5218@gmail.com>
2022-05-26 10:29:27 -0700

    Change api keys

--- public/js/trendy_recipes.js
+++ public/js/trendy_recipes.js
@@ -43,15 +43,8 @@ async function Trendy_contents_change_Dessert() {
     })
 }
 
-var getCookie = function (name) {
-    var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-    return value ? value[2] : null;
-};
-
-
 function setup() {
-    let texttest = getCookie("userNickName")
-    $("#name-goes-here2").html(texttest+"!")
+    Trendy_contents_change_Main()
 }
 
 $(document).ready(setup)
